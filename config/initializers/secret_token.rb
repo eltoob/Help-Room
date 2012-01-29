471e73679ef0e9aaa77372c320e2e490224b955b
Elie Toubiana <elie.toubiana@gmail.com>
2012-01-28 23:47:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helproom::Application.config.secret_token = '548e4511b0815ad3b2aefb07f0561e8c299f4d186171d4f38352ced7684173d43f653d3a072c44e4c2777ad403621a6bddc7bf7f076ff15d244229de7cbab7af'
