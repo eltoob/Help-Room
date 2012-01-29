a147d4260386638e44883555e4559347f309710c
Dina Lamdany <dinalamdany@gmail.com>
2012-01-29 02:34:11 -0500

    just columbia.edu regex

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -107,7 +107,7 @@ Devise.setup do |config|
   # Email regex used to validate email formats. It simply asserts that
   # an one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
-  config.email_regexp = /\A[a-z]{3}\d{4}+@columbia+\.edu/i
+  config.email_regexp = /\A[\w+\-.]+@columbia+\.edu/i
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
