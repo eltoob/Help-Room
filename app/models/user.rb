ba3f121bf4a9d0be2046b5033eb5f29e5d98e0be
Dina Lamdany <dinalamdany@gmail.com>
2012-01-29 01:12:20 -0500

    for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 
   validates_presence_of :name
-  validates_uniqueness_of :name, :email, :case_sensitive => false
+  validates_uniqueness_of :name, :case_sensitive => false
 
 end
