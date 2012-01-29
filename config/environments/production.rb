fdf170662cdaf1a01bafc424f4462dfed84dd845
Dina Lamdany <dinalamdany@gmail.com>
2012-01-29 14:04:41 -0500

    Changed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Helproom::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'yourhost.com' }
+  config.action_mailer.default_url_options = { :host => 'helproom.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
