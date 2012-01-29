4305cbf82b5853c2417dd504c01865fd818ac03e
Dina Lamdany <dinalamdany@gmail.com>
2012-01-29 13:45:43 -0500

    Modified devise.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.sendgrid.net",
   :port                 => 587,
   :domain               => "heroku.com",
-  :user_name            => ENV['SENDGRID_USERNAME']
-  :password             => ENV['SENDGRID_PASSWORD']
+  :user_name            => ENV['SENDGRID_USERNAME'],
+  :password             => ENV['SENDGRID_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
