6e91aa63e3da097ddcac841d5af6fbd051f4a68a
Dina Lamdany <dinalamdany@gmail.com>
2012-02-03 00:43:17 -0500

    removed excess db files

--- db/migrate/20120129051313_add_nameto_users.rb
+++
@@ -1,7 +0,0 @@
-class AddNametoUsers < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20120129232431_add_username_to_wait_line.rb
+++
@@ -1,5 +0,0 @@
-class AddUsernameToWaitLine < ActiveRecord::Migration
-  def change
-    add_column :wait_lines, :username, :string
-  end
-end
